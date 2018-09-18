7d6a82f35d46b5c33c0104bef30abef415b31332
RScool <739646645@qq.com>
2018-09-18 19:34:06 +0800

    修复弹幕时间与添加评论计数

--- pages/index/main/video/video.js
+++ pages/index/main/video/video.js
@@ -5,6 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+<<<<<<< Updated upstream
       video:{},
       comment:{},
       value:null,
@@ -12,62 +13,88 @@ Page({
       open: null,
       currentTime:0,
       danmuList: []
+=======
+    video: {},
+    comment: {},
+    value: null,
+    userInfo: {},
+    open: null,
+    currentTime: 0,
+    danmuList: [],
+    videoTime:0
+
+>>>>>>> Stashed changes
   },
-  
+
 
   //获取视频进度
-  getCurrentTime:function(e){
-    
+  getCurrentTime: function(e) {
+    let sTime = e.detail.currentTime
     this.setData({
-      currentTime:e.detail.currentTime
+      currentTime: sTime
     })
   },
 
-  //获取输入的值
-  getInputVal: function (e) {
-    let val = e.detail.value
+  //评论时视频的进度
+  getVideoTime:function(){
+      let sTime =this.data.currentTime
+      console.log(sTime)
       this.setData({
-        value: val
+        videoTime:sTime
       })
   },
 
+  //获取输入的值
+  getInputVal: function(e) {
+    let val = e.detail.value
+    this.setData({
+      value: val
+    })
+  },
+
   //提交评论
-  subComment:function(){
-    var that =this;
-    if(this.data.value){
+  subComment: function() {
+    var that = this;
+    if (this.data.value) {
       wx.request({
         url: 'https://www.mosillion.top/TestSSM/comment/submitCommment',
-        data:{
-          id:this.data.video.id,
+        data: {
+          id: this.data.video.id,
           username: this.data.userInfo.nickName,
           userImg: this.data.userInfo.avatarUrl,
-          content:this.data.value,
-          wxNum:this.data.open,
-          currentTime:this.data.currentTime
+          content: this.data.value,
+          wxNum: this.data.open,
+          currentTime: this.data.videoTime
         },
-        success:function(res){
+        success: function(res) {
           wx.showToast({
             title: '发表成功',
             icon: 'none',
             duration: 2000
           })
 
-          let comment={
+          let comment = {
             username: that.data.userInfo.nickName,
             userImg: that.data.userInfo.avatarUrl,
             subTime: new Date(),
-            content:that.data.value
+            content: that.data.value
           }
-          let newCommetList=that.data.comment
+          let newCommetList = that.data.comment
           newCommetList.unshift(comment)
+
+          let newVideo=that.data.video
+
+          newVideo.commentNum = parseInt(that.data.video.commentNum) +1
+
           that.setData({
-            value:'',
-            comment:newCommetList
+            value: '',
+            comment: newCommetList,
+            video:newVideo
           })
         }
       })
 
-    }else{
+    } else {
       wx.showToast({
         title: '评论不能为空',
         icon: 'none',
@@ -77,34 +104,34 @@ Page({
   },
 
   //获取评论
-  getComment:function(id){
-    let commentList=[];
-    let danmuList=[];
-    let that=this
+  getComment: function(id) {
+    let commentList = [];
+    let danmuList = [];
+    let that = this
     wx.request({
       url: 'https://www.mosillion.top/TestSSM/comment/getComment',
-      data:{
-        id:id
+      data: {
+        id: id
       },
-      success:function(res){
+      success: function(res) {
         for (let i = res.data.length - 1, j = 0; i >= 0; --i, ++j) {
           let content = res.data[i]
 
-          let comment ={}
-          comment.username=content.username
+          let comment = {}
+          comment.username = content.username
           comment.subTime = content.commentTime
           comment.userImg = content.userImg
           comment.content = content.commentContent
-          commentList[j]=comment
+          commentList[j] = comment
 
           let danmu = {}
           danmu.text = content.commentContent
           danmu.time = parseInt(content.curTime)
-          danmuList[j]=danmu
+          danmuList[j] = danmu
         }
         that.setData({
-          comment:commentList,
-          danmuList:danmuList
+          comment: commentList,
+          danmuList: danmuList
         })
       }
     })
@@ -113,21 +140,21 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (e) {
-    var vname=JSON.parse(e.video)
+  onLoad: function(e) {
+    var vname = JSON.parse(e.video)
     // console.log(vname)
     this.setData({
-      video:vname
+      video: vname
     })
 
     let that = this
     // 查看是否授权
     wx.getSetting({
-      success: function (res) {
+      success: function(res) {
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称
           wx.getUserInfo({
-            success: function (res) {
+            success: function(res) {
               that.setData({
                 userInfo: res.userInfo,
               })
@@ -140,7 +167,7 @@ Page({
 
     wx.getStorage({
       key: 'user',
-      success: function (res) {
+      success: function(res) {
         that.setData({
           open: res.data.openId
         })
@@ -153,49 +180,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
